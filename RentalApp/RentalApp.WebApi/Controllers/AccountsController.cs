fb1ee4da6647ec3eb97bb58b6c2bc809b8b49dbf
adamek195 <248955@student.pwr.edu.pl>
2022-04-07 21:29:57 +0200

    change acount controller

--- RentalApp/RentalApp.WebApi/Controllers/AccountsController.cs
+++ RentalApp/RentalApp.WebApi/Controllers/AccountsController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 using RentalApp.Application.Interfaces;
 using RentalApp.WebApi.Filters;
+using Microsoft.AspNetCore.Authorization;
+using RentalApp.Application.Dto.Users;
 
 namespace RentalApp.WebApi.Controllers
 {
@@ -21,13 +23,17 @@ namespace RentalApp.WebApi.Controllers
             _usersService = usersService;
         }
 
-        [HttpGet]
-        [SwaggerOperation(Summary = "Return all users")]
-        public async Task<IActionResult> GetUsers()
+        [HttpPost]
+        [Route("Register")]
+        [AllowAnonymous]
+        [SwaggerOperation(Summary = "Registering an account in the app")]
+        public async Task<IActionResult> Register([FromBody] CreateUserDto newUserDto)
         {
-            var users = await _usersService.GetUsers();
+            //var newUser = await _usersService.CreateUser(newUserDto);
 
-            return Ok(users);
+            //return Created($"api/users/{newUser.Id}", newUser);
+
+            return Created($"api/users", newUserDto);
         }
     }
 }
---
+++ RentalApp/RentalApp.WebApi/Installers/AuthenticationInstaller.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using RentalApp.Domain.Entities;
+using RentalApp.Infrastructure.Data;
+
+namespace RentalApp.WebApi.Installers
+{
+    public class AuthenticationInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddIdentityCore<User>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<RentalAppContext>();
+
+            var key = Encoding.ASCII.GetBytes(Configuration["JwtToken:Key"]);
+
+            services.AddAuthentication(options =>
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidIssuer = Configuration["JwtToken:TokenIssuer"],
+                        ValidateAudience = false,
+                        RequireExpirationTime = true
+                    };
+                });
+        }
+    }
+}
