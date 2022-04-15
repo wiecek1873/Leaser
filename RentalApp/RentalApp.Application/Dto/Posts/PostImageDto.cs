829066a92eab07317f2af4db44e604e6c9b7c6da
Michał Więcek <wiecek1873@gmail.com>
2022-04-16 01:28:13 +0200

    Added image to PostImageDto

--- RentalApp/RentalApp.Application/Dto/Posts/PostImageDto.cs
+++ RentalApp/RentalApp.Application/Dto/Posts/PostImageDto.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Microsoft.AspNetCore.Http;
 
 namespace RentalApp.Application.Dto.Posts
 {
 	class PostImageDto
 	{
+		public IFormFile PostImage { get; set; }
 	}
 }
