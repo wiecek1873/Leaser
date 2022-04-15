9e7c54cda8be398ded262084b4a2cd763ed80ad9
Michał Więcek <wiecek1873@gmail.com>
2022-04-16 01:58:29 +0200

    Added DepositStatuesRepository

---
+++ RentalApp/RentalApp.Domain/Interfaces/IDepositStatusesRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using RentalApp.Domain.Entities;
+
+namespace RentalApp.Domain.Interfaces
+{
+	public interface IDepositStatusesRepository
+	{
+		Task<DepositStatus> GetDepositStatus(int depositStatusId);
+		Task<DepositStatus> AddDepositStatus(DepositStatus newDepositStatus);
+		Task UpdateDepositStatus(int depositStatusId, DepositStatus updatedDepositStatus);
+	}
+}
