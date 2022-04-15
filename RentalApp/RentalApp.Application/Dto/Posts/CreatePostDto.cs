3cd6b653a594679f4061aa1efee05e27724a44d9
Michał Więcek <wiecek1873@gmail.com>
2022-04-16 00:32:30 +0200

    Minor changes in post dtos

--- RentalApp/RentalApp.Application/Dto/Posts/CreatePostDto.cs
+++ RentalApp/RentalApp.Application/Dto/Posts/CreatePostDto.cs
@@ -1,23 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace RentalApp.Application.Dto.Posts
 {
 	public class CreatePostDto
 	{
-		public int Id { get; set; }
+		public string UserId { get; set; }
 		public int CategoryId { get; set; }
-        public string UserId { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public int DepositId { get; set; }
-        public double Price { get; set; }
-        public double? PricePerWeek { get; set; }
-        public double? PricePerMonth { get; set; }
-        public DateTime AvailableFrom { get; set; }
-        public DateTime? AvailableTo { get; set; }
-    }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public int DepositValue { get; set; }
+		public double Price { get; set; }
+		public double? PricePerWeek { get; set; }
+		public double? PricePerMonth { get; set; }
+		public DateTime AvailableFrom { get; set; }
+		public DateTime? AvailableTo { get; set; }
+		public CreatePostImageDto CreatePostImageDto { get; set; }
+	}
 }
--- RentalApp/RentalApp.Application/Dto/Posts/PostImageDto.cs
+++ RentalApp/RentalApp.Application/Dto/Posts/PostImageDto.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 
 namespace RentalApp.Application.Dto.Posts
 {
-	public class PostImageDto
+	class PostImageDto
 	{
-		public IFormFile PostImage { get; set; }
 	}
 }
