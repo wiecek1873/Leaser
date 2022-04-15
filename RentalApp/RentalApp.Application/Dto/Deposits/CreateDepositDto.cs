7619d27ac0e1c4c1e83d41cb275f95d4a58dd77b
Michał Więcek <wiecek1873@gmail.com>
2022-04-16 00:49:27 +0200

    Added data validation

--- RentalApp/RentalApp.Application/Dto/Deposits/CreateDepositDto.cs
+++ RentalApp/RentalApp.Application/Dto/Deposits/CreateDepositDto.cs
@@ -1,7 +1,10 @@
-﻿namespace RentalApp.Application.Dto.Deposits
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentalApp.Application.Dto.Deposits
 {
 	public class CreateDepositDto
 	{
+		[Required(ErrorMessage = "Value is required.")]
 		public int Value { get; set; }
 	}
 }
