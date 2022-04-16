6bc06cad15ddac9a543c8e4889f5e5c85cf1d139
Michał Więcek <wiecek1873@gmail.com>
2022-04-16 02:17:07 +0200

    Added deposit status service

---
+++ RentalApp/RentalApp.Application/Interfaces/IDepositStatusesService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using RentalApp.Application.Dto.DepositStatuses;
+
+namespace RentalApp.Application.Interfaces
+{
+	public interface IDepositStatusesService
+	{
+		Task<DepositStatusDto> GetDepositStatus(int depositStatusId);
+		Task<DepositStatusDto> CreateDepositStatus(CreatDepositStatusDto newDepositStatusDto);
+		public Task UpdateDepositStatus(int depositStatusId, CreatDepositStatusDto updatedDepositStatusDto);
+	}
+}
---
+++ RentalApp/RentalApp.Application/Services/DepositStatusesService.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using System.Threading.Tasks;
+using RentalApp.Application.Exceptions;
+using RentalApp.Application.Dto.DepositStatuses;
+using RentalApp.Application.Interfaces;
+using RentalApp.Domain.Interfaces;
+using RentalApp.Domain.Entities;
+
+namespace RentalApp.Application.Services
+{
+	public class DepositStatusesService : IDepositStatusesService
+	{
+		private readonly IDepositStatusesRepository _depositStatusesRepository;
+		private readonly IMapper _mapper;
+
+		public DepositStatusesService(IDepositStatusesRepository depositStatusesRepository, IMapper mapper)
+		{
+			_depositStatusesRepository = depositStatusesRepository;
+			_mapper = mapper;
+		}
+
+		public async Task<DepositStatusDto> GetDepositStatus(int depositStatusId)
+		{
+			var depositStatus = await _depositStatusesRepository.GetDepositStatus(depositStatusId);
+
+			if (depositStatus == null)
+				throw new NotFoundException("Deposit status with this id does not exist.");
+
+			return _mapper.Map<DepositStatusDto>(depositStatus);
+		}
+
+		public async Task<DepositStatusDto> CreateDepositStatus(CreatDepositStatusDto newDepositStatusDto)
+		{
+			var depositStatusToAdd = _mapper.Map<DepositStatus>(newDepositStatusDto);
+
+			await _depositStatusesRepository.AddDepositStatus(depositStatusToAdd);
+
+			return _mapper.Map<DepositStatusDto>(depositStatusToAdd);
+		}
+
+		public async Task UpdateDepositStatus(int depositStatusId, CreatDepositStatusDto updatedDepositStatusDto)
+		{
+			var depositStatusToUpdate = await _depositStatusesRepository.GetDepositStatus(depositStatusId);
+
+			if (depositStatusToUpdate == null)
+				throw new NotFoundException("Deposit status with this id does not exist.");
+
+			depositStatusToUpdate = _mapper.Map<DepositStatus>(updatedDepositStatusDto);
+
+			await _depositStatusesRepository.UpdateDepositStatus(depositStatusId, depositStatusToUpdate);
+		}
+	}
+}
