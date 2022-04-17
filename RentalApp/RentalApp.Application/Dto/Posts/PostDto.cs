ea7fcabb4a472cdbf0b82f42d796c17915923b3e
adamek195 <248955@student.pwr.edu.pl>
2022-04-17 17:45:00 +0200

    set nullable depositId in dto

--- RentalApp/RentalApp.Application/Dto/Posts/PostDto.cs
+++ RentalApp/RentalApp.Application/Dto/Posts/PostDto.cs
@@ -9,7 +9,7 @@ namespace RentalApp.Application.Dto.Posts
 		public Guid UserId { get; set; }
 		public string Title { get; set; }
 		public string Description { get; set; }
-		public int DepositId { get; set; }
+		public int? DepositId { get; set; }
 		public double Price { get; set; }
 		public double? PricePerWeek { get; set; }
 		public double? PricePerMonth { get; set; }
