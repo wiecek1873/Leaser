d666a4f0256c07089a2a2a57f418b2f95525b006
Michał Więcek <wiecek1873@gmail.com>
2022-04-20 16:19:11 +0200

    Category interface

---
+++ RentalApp/RentalApp.Domain/Interfaces/ICategoriesRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using RentalApp.Domain.Entities;
+
+namespace RentalApp.Domain.Interfaces
+{
+	interface ICategoriesRepository
+	{
+		Task<Category> GetCategory(int categoryId);
+		Task<Category> AddCategory(Category newCategory);
+		Task UpdateCategory(int categoryId, Category updatedCategory);
+	}
+}
