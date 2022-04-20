0e363a87736c17ac251fa45b1828e581409dc2b2
Michał Więcek <wiecek1873@gmail.com>
2022-04-20 16:35:42 +0200

    Category dtos

---
+++ RentalApp/RentalApp.Application/Dto/Categories/CategoryDto.cs
@@ -0,0 +1,7 @@
+﻿namespace RentalApp.Application.Dto.Categories
+{
+	public class CategoryDto
+	{
+		public string CategoryName { get; set; }
+	}
+}
---
+++ RentalApp/RentalApp.Application/Dto/Categories/RequestCategoryDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentalApp.Application.Dto.Categories
+{
+	public class RequestCategoryDto
+	{
+		[Required(ErrorMessage = "Category name is required.")]
+		public string CategoryName { get; set; }
+	}
+}
