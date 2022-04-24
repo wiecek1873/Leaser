2169452156d42986fa88ba0b536db0b24abd166b
Michał Więcek <wiecek1873@gmail.com>
2022-04-24 16:59:41 +0200

    Consistent accessibility

--- RentalApp/RentalApp.Application/Interfaces/IDepositsService.cs
+++ RentalApp/RentalApp.Application/Interfaces/IDepositsService.cs
@@ -7,7 +7,7 @@ namespace RentalApp.Application.Interfaces
 	{
 		Task<DepositDto> GetDeposit(int depositId);
 		Task<DepositDto> CreateDeposit(RequestDepositDto newDepositDto);
-		public Task UpdateDeposit(int depositId, RequestDepositDto updatedDepositDto);
+		Task UpdateDeposit(int depositId, RequestDepositDto updatedDepositDto);
 		Task DeleteDeposit(int depositId);
 	}
 }
