eebbcdf6419ad07b3d97a3f22708b489dc0bedb3
Michał Więcek <wiecek1873@gmail.com>
2022-04-24 18:49:17 +0200

    Repository to payments

---
+++ RentalApp/RentalApp.Infrastructure/Repositories/PaymentsRepository.cs
@@ -0,0 +1,58 @@
+﻿using RentalApp.Domain.Interfaces;
+using RentalApp.Domain.Entities;
+using System.Threading.Tasks;
+using RentalApp.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace RentalApp.Infrastructure.Repositories
+{
+	public class PaymentsRepository : IPaymentsRepository
+	{
+		private readonly RentalAppContext _rentalAppContext;
+
+		public PaymentsRepository(RentalAppContext rentalAppContext)
+		{
+			_rentalAppContext = rentalAppContext;
+		}
+
+		public async Task<Payment> GetPayment(int paymentId)
+		{
+			var payment = await _rentalAppContext.Payments.SingleOrDefaultAsync(p => p.Id == paymentId);
+
+			return payment;
+		}
+
+		public async Task<Payment> AddPayment(Payment newPayment)
+		{
+			_rentalAppContext.Payments.Add(newPayment);
+			await _rentalAppContext.SaveChangesAsync();
+
+			return newPayment;
+		}
+
+		public async Task UpdatePayment(int paymentId, Payment updatedPayment)
+		{
+			var paymentToUpdate = await _rentalAppContext.Payments.SingleOrDefaultAsync(p => p.Id == paymentId);
+
+			if(paymentToUpdate != null)
+			{
+				paymentToUpdate.Bank = updatedPayment.Bank;
+				paymentToUpdate.CardNumber = updatedPayment.CardNumber;
+				paymentToUpdate.ExpirationDate = updatedPayment.ExpirationDate;
+				paymentToUpdate.CVV = updatedPayment.CVV;
+			}
+
+			await _rentalAppContext.SaveChangesAsync();
+		}
+
+		public async Task DeletePayment(int paymentId)
+		{
+			var paymentToDelete = await _rentalAppContext.Payments.SingleOrDefaultAsync(p => p.Id == paymentId);
+
+			if (paymentToDelete != null)
+				_rentalAppContext.Payments.Remove(paymentToDelete);
+
+			await _rentalAppContext.SaveChangesAsync();
+		}
+	}
+}
