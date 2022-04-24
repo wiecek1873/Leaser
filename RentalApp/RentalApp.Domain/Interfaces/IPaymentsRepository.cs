3124fc4a73e1b16f025b15ac3672a6abca7c5b36
Michał Więcek <wiecek1873@gmail.com>
2022-04-24 18:38:50 +0200

    Payments repository interface

---
+++ RentalApp/RentalApp.Domain/Interfaces/IPaymentsRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using RentalApp.Domain.Entities;
+
+namespace RentalApp.Domain.Interfaces
+{
+	public interface IPaymentsRepository
+	{
+		Task<Payment> GetPayment(int paymentId);
+		Task<Payment> AddPayment(Payment newPayment);
+		Task UpdatePayment(int paymentId, Payment updatedPayment);
+		Task DeletePayment(int paymentId);
+	}
+}
