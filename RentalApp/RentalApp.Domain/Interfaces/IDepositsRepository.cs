c6609ac913cc8bc7e06d4f60193e493f9c4c0a4a
Michał Więcek <wiecek1873@gmail.com>
2022-04-25 16:13:30 +0200

    Added spaces to interfaces

--- RentalApp/RentalApp.Domain/Interfaces/IDepositsRepository.cs
+++ RentalApp/RentalApp.Domain/Interfaces/IDepositsRepository.cs
@@ -6,8 +6,11 @@ namespace RentalApp.Domain.Interfaces
 	public interface IDepositsRepository
 	{
 		Task<Deposit> GetDeposit(int depositId);
+
 		Task<Deposit> AddDeposit(Deposit newDeposit);
+
 		Task UpdateDeposit(int depositId, Deposit updatedDeposit);
+
 		Task DeleteDeposit(int depositId);
 	}
 }
