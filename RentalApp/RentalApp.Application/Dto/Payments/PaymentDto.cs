8727d3d7a53939c4785ef86cc2537708e4bd7a81
Michał Więcek <wiecek1873@gmail.com>
2022-04-28 22:33:35 +0200

    Payment DTOs

---
+++ RentalApp/RentalApp.Application/Dto/Payments/PaymentDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RentalApp.Application.Dto.Payments
+{
+	class PaymentDto
+	{
+		public int Id { get; set; }
+
+		public Guid UserId { get; set; }
+
+		public string Bank { get; set; }
+
+		public string CardNumber { get; set; }
+
+		public string ExpirationDate { get; set; }
+
+		public string CVV { get; set; }
+	}
+}
---
+++ RentalApp/RentalApp.Application/Dto/Payments/RequestPaymentDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RentalApp.Application.Dto.Payments
+{
+	public class RequestPaymentDto
+	{
+		[Required(ErrorMessage = "User id is required.")]
+		public Guid UserId { get; set; }
+
+		[Required(ErrorMessage = "Bank is required.")]
+		public string Bank { get; set; }
+
+		[Required(ErrorMessage = "Card number is required.")]
+		public string CardNumber { get; set; }
+
+		[Required(ErrorMessage = "Expiration date is required.")]
+		public string ExpirationDate { get; set; }
+
+		[Required(ErrorMessage = "CVV is required.")]
+		public string CVV { get; set; }
+	}
+}
