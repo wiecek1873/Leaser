4c06ef0a6e39544ea66d11da12aeb7d5f9142597
Michał Więcek <wiecek1873@gmail.com>
2022-04-29 15:22:09 +0200

    Added image url to category entity

---
+++ RentalApp/RentalApp.Infrastructure/Migrations/20220429132035_ImageURLInCategory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RentalApp.Infrastructure.Migrations
+{
+    public partial class ImageURLInCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "Categories");
+        }
+    }
+}
