65672d38306629c16e7395256d7fb12060f6de53
adamek195 <248955@student.pwr.edu.pl>
2022-05-05 18:28:39 +0200

    add space in category dto

--- RentalApp/RentalApp.Application/Dto/Categories/RequestCategoryDto.cs
+++ RentalApp/RentalApp.Application/Dto/Categories/RequestCategoryDto.cs
@@ -6,6 +6,7 @@ namespace RentalApp.Application.Dto.Categories
 	{
 		[Required(ErrorMessage = "Category name is required.")]
 		public string CategoryName { get; set; }
+
 		[Required(ErrorMessage ="Category image URL is required.")]
 		public string ImageURL { get; set; }
 	}
