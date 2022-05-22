505bcc2de2c56274a8f96b8559061bc6b5343f6f
adamek195 <248955@student.pwr.edu.pl>
2022-05-22 10:07:53 +0200

    improve script

--- scripts/categories.sql
+++ scripts/categories.sql
@@ -1,14 +1,16 @@
+DELETE FROM Categories;
+
 INSERT INTO Categories(CategoryName, ImageURL)
-VALUES 
-('Clothes', 'https://images.unsplash.com/photo-1542060748-10c28b62716f'), 
-('Sports', 'https://images.unsplash.com/photo-1512412046876-f386342eddb3'), 
-('Cameras', 'https://images.unsplash.com/photo-1502982720700-bfff97f2ecac'), 
-('Tools', 'https://images.unsplash.com/photo-1563090728-6f7cdbfb4d72'), 
-('Medicine', 'https://images.unsplash.com/photo-1603398938378-e54eab446dde'), 
-('Drones', 'https://images.unsplash.com/photo-1521405924368-64c5b84bec60'), 
-('Garden', 'https://images.unsplash.com/photo-1621460248083-6271cc4437a8'	), 
-('Electronic', 'https://images.unsplash.com/photo-1615655406736-b37c4fabf923'), 
-('Toys', 'https://images.unsplash.com/photo-1558060370-d644479cb6f7'), 
-('Games', 'https://images.unsplash.com/photo-1632501641765-e568d28b0015'), 
-('Parties', 'https://images.unsplash.com/photo-1570102881689-c04ab4cf1f4c'), 
-('Bikes', 'https://images.unsplash.com/photo-1571068316344-75bc76f77890'); 
+VALUES
+('Clothes', 'https://images.unsplash.com/photo-1542060748-10c28b62716f'),
+('Sports', 'https://images.unsplash.com/photo-1512412046876-f386342eddb3'),
+('Cameras', 'https://images.unsplash.com/photo-1502982720700-bfff97f2ecac'),
+('Tools', 'https://images.unsplash.com/photo-1563090728-6f7cdbfb4d72'),
+('Medicine', 'https://images.unsplash.com/photo-1603398938378-e54eab446dde'),
+('Drones', 'https://images.unsplash.com/photo-1521405924368-64c5b84bec60'),
+('Garden', 'https://images.unsplash.com/photo-1621460248083-6271cc4437a8'	),
+('Electronic', 'https://images.unsplash.com/photo-1615655406736-b37c4fabf923'),
+('Toys', 'https://images.unsplash.com/photo-1558060370-d644479cb6f7'),
+('Games', 'https://images.unsplash.com/photo-1632501641765-e568d28b0015'),
+('Parties', 'https://images.unsplash.com/photo-1570102881689-c04ab4cf1f4c'),
+('Bikes', 'https://images.unsplash.com/photo-1571068316344-75bc76f77890');
